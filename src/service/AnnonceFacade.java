1bbfef9b0b6de827032a67e5300c5d7c4388e08a
medhatoch <medamine.hatoch@gmail.com>
2017-01-05 19:38:17 +0000

    initial commit

---
+++ src/service/AnnonceFacade.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import bean.Annonce;
+import bean.AnnonceItem;
+import bean.Compte;
+import bean.Quartier;
+import bean.Ville;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author lenovo
+ */
+public class AnnonceFacade extends AbstractFacade<Annonce> {
+
+    public AnnonceFacade() {
+        super(Annonce.class);
+    }
+    private CompteFacade compteFacade = new CompteFacade();
+    private AnnonceItemFacade annonceItemFacade = new AnnonceItemFacade();
+
+    /* private void createAnnonce(Long code, String titre, int prix, Annonce an, Ville ville, Quartier quartier, Compte c){
+        Compte compte = compteFacade.find(code);
+        if(c!=null){
+            Annonce annonce = new Annonce(titre, prix, ville, quartier);
+            create(annonce);
+        }
+        
+    }*/
+    public void save(Annonce annonce, List<AnnonceItem> annonceItems) {
+        create(annonce);
+        for (AnnonceItem annonceItem : annonceItems) {
+            annonceItem.setAnnonce(annonce);
+            annonceItem.setId(annonceItemFacade.generateId("AnnonceItem", "id"));
+            annonceItemFacade.create(annonceItem);
+        }
+    }
+
+    public List<Annonce> FindAnnonce(String titre, Ville ville, Quartier quartier) {
+        String requette = "SELECT annonce FROM Annonce annonce WHERE 1=1";
+        if (!"".equals(titre)) {
+            requette += " AND annonce.titre='" + titre + "'";
+        }
+        if (ville != null) {
+            requette += " AND annonce.ville.id=" + ville.getId();
+        }
+        if (quartier != null && quartier.getId() != null) {
+            requette += " AND annonce.quartier.id=" + quartier.getId();
+        }
+        return getEntityManager().createQuery(requette).getResultList();
+    }
+    
+    public List<Annonce> FindAnnonceAvance(String titre,Ville ville,Quartier quartier,Compte compte,Date datcre){
+        String requette = "SELECT annonce FROM Annonce annonce WHERE 1=1";
+        if(!"".equals(titre)){
+           requette += " AND annonce.titre='"+ titre + "'";
+        }
+        if (ville != null) {
+            requette += " AND annonce.ville.id=" + ville.getId();
+        }
+        if (quartier != null && quartier.getId() != null) {
+            requette += " AND annonce.quartier.id=" + quartier.getId();
+        }
+        if (compte != null) {
+            requette += " AND annonce.compte.id=" + compte.getId();
+        }if(datcre !=null){
+            requette += " AND annonce.datecre='"+ datcre +"'";
+        }
+        
+        return getEntityManager().createQuery(requette).getResultList();
+    }
+
+    public List<Compte> findCompteByAnnonce(Annonce annonce) {
+        return getEntityManager().createQuery("SELECT c.compte FROM Annonce c WHERE c.id=" + annonce.getId()).getResultList();
+    }
+
+    public List<Annonce> findAnnonceByCompte(Compte compte) {
+        return getEntityManager().createQuery("SELECT a FROM Annonce a WHERE a.compte.id=" + compte.getId()).getResultList();
+    }
+
+    public void modifyAnnonce(Annonce annonce, String titre,Ville ville,Quartier quartier,int prix,List<AnnonceItem> annonceItems) {
+        annonce.setPrix(prix);
+        annonce.setVille(ville);
+        annonce.setQuartier(quartier);
+        annonce.setTitre(titre);
+        for (AnnonceItem annonceItem : annonceItems) {
+            annonceItem.setAnnonce(annonce);
+            annonceItemFacade.edit(annonceItem);
+        }
+        edit(annonce);
+    }
+
+    public void deleteAnnonce(Annonce annonce) {
+        List<AnnonceItem> annonceItems = new ArrayList<>();
+        annonceItems = annonceItemFacade.findAnnonceItemByAnnonce(annonce);
+        for (AnnonceItem annonceItem : annonceItems) {
+            annonceItemFacade.remove(annonceItem);
+        }
+        remove(annonce);
+    }
+    
+    public void deleteCompte(Compte compte){
+        List<Annonce> annonces = new ArrayList<>();
+        for (Annonce annonce : annonces) {
+            deleteAnnonce(annonce);
+        }
+        compteFacade.remove(compte);
+    }
+
+}
---
+++ src/view/ModifyAnnonceController.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import bean.Annonce;
+import bean.AnnonceItem;
+import bean.Compte;
+import bean.Metier;
+import bean.Quartier;
+import bean.Ville;
+import helper.AnnonceFxHelper;
+import helper.AnnonceItemFxHelper;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import service.AnnonceFacade;
+import service.AnnonceItemFacade;
+import service.CompteFacade;
+import service.MetierFacade;
+import service.QuartierFacade;
+import service.VilleFacade;
+
+/**
+ * FXML Controller class
+ *
+ * @author lenovo
+ */
+public class ModifyAnnonceController implements Initializable {
+
+   /**
+     * Initializes the controller class.
+     */
+    @FXML
+    private TextField titre;
+    @FXML
+    private ComboBox<Compte> compte= new ComboBox<>();
+    @FXML
+    private TextField prix;
+    @FXML
+    private TextField nombre;
+    @FXML
+    private ComboBox<Ville> ville = new ComboBox<>();
+    @FXML
+    private ComboBox<Quartier> quartier = new ComboBox<>();
+    @FXML
+    private ComboBox<Metier> metier = new ComboBox<>();
+    @FXML
+    private ComboBox<Annonce> annonce = new ComboBox<>();
+    @FXML
+    private TableView annonceItem = new TableView();
+    
+    AnnonceFacade annonceFacade = new AnnonceFacade();
+    private AnnonceItemFxHelper annonceItemFxHelper ;
+    private AnnonceFxHelper annonceFxHelper;
+    private VilleFacade villeFacade = new VilleFacade();
+    private QuartierFacade quartierFacade = new QuartierFacade();
+    private MetierFacade metierFacade = new MetierFacade();
+    private CompteFacade compteFacade = new CompteFacade();
+    private AnnonceItemFacade annonceItemFacade=new AnnonceItemFacade();
+    
+    
+    private AnnonceItem getParamAnnonceItem() {
+        AnnonceItem annonceIte;
+        annonceIte = new AnnonceItem(metier.getSelectionModel().getSelectedItem(),Integer.parseInt(nombre.getText()));
+        return annonceIte;
+    }
+    
+    
+    private Annonce getParamAnnonce(){
+        Annonce annonce = new Annonce(annonceFacade.generateId("Annonce", "id"),titre.getText(),Integer.parseInt(prix.getText()), ville.getValue(), quartier.getValue(),compte.getSelectionModel().getSelectedItem());
+        return annonce;
+    }
+ 
+    @FXML
+    public void modifierAnnonce(ActionEvent actionEvent) throws IOException{
+        annonceFacade.modifyAnnonce(annonce.getValue(), titre.getText(), ville.getValue(), quartier.getValue(),Integer.parseInt(prix.getText()), annonceItem.getItems());
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information");
+            alert.setHeaderText(null);
+            alert.setContentText("L'annonce a ete modifie ! ");
+            alert.showAndWait();
+            ViewLauncher.forward(actionEvent, "ModifyAnnonce.fxml", this.getClass());
+    }
+    
+    public void remplirAnnonce(Event event){
+        titre.setText(annonce.getValue().getTitre());
+        prix.setText(""+annonce.getValue().getPrix());
+        ville.setValue(annonce.getValue().getVille());
+        quartier.setValue(annonce.getValue().getQuartier());
+        annonceItemFxHelper.getTable().getItems().setAll((annonce.getValue().getAnnonceItems()));
+    }
+    
+    @FXML
+    public void saveAnnonceItem(ActionEvent actionEvent){
+                AnnonceItem annonceItem = getParamAnnonceItem();
+            annonceItemFxHelper.getTable().getItems().add(annonceItem);
+    }
+    
+    public void deleteAnnonceItem(ActionEvent actionEvent){
+        int index =  this.annonceItem.getSelectionModel().getSelectedIndex();
+        annonceItemFxHelper.getTable().getItems().remove(index);
+    }
+    
+    AnnonceItem a = new AnnonceItem();
+    @FXML
+    public void getAnnonceItem(Event event){
+        AnnonceItem a = (AnnonceItem) annonceItem.getSelectionModel().getSelectedItem();
+    }
+    
+    public void initComboBox() {
+        ville.setItems(FXCollections.observableArrayList(villeFacade.findAll()));
+        ville.getSelectionModel().select(0);
+        quartier.setItems(FXCollections.observableArrayList(quartierFacade.findAll()));
+        metier.setItems(FXCollections.observableArrayList(metierFacade.findAll()));
+        compte.setItems(FXCollections.observableArrayList(compteFacade.findAll()));
+        annonce.setItems(FXCollections.observableArrayList(annonceFacade.findAll()));
+    }
+    
+    public void initHelper(){
+        annonceItemFxHelper = new AnnonceItemFxHelper(annonceItem);
+    }
+
+    @FXML
+    public void findQuartierByVille(Event event) {
+        Ville v = ville.getSelectionModel().getSelectedItem();
+        quartier.setItems(FXCollections.observableArrayList(quartierFacade.findQuartierByVille(v)));
+    }
+    
+    @FXML
+    public void findAnnonceByCompte(Event event){
+       Compte c = compte.getSelectionModel().getSelectedItem();
+       annonce.setItems(FXCollections.observableArrayList(annonceFacade.findAnnonceByCompte(c)));
+    }
+    
+    public void annuler(ActionEvent actionEvent) throws IOException{
+        ViewLauncher.forward(actionEvent, "Main.fxml", this.getClass());
+    }
+    
+    public void initialize(URL url, ResourceBundle rb) {
+        initComboBox();
+        initHelper();
+        // TODO
+    }    
+    
+}
